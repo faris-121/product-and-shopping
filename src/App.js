7d13aea20d89b735884b520637f83256009cbb3c
faris-121 <farissalman300@gmail.com>
2024-12-29 18:41:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductForm from './components/ProductForm';
+import ProductSearch from './components/ProductSearch';
+import ShoppingUI from './components/ShoppingUI';
+
+
+
+
+function App() {
+  return (
+    <div>
+      <ProductForm />
+      <ProductSearch />
+      <ShoppingUI />
+     
+      
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/ShoppingUI.jsx
@@ -0,0 +1,130 @@
+import React from 'react';
+
+const ShoppingUI = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="row g-3">
+                            
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card">
+                                    <img src="https://m.media-amazon.com/images/I/61Ol4v72-NL._AC_UY327_FMwebp_QL65_.jpg" className="card-img-top" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Gaming laptop</h5>
+                                        <p className="card-text">$59000</p>
+                                        <a href="#" className="btn btn-primary">BUY NOW</a>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://m.media-amazon.com/images/I/61nTNphSBvL._AC_UY327_FMwebp_QL65_.jpg" className="card-img-top" alt="Card 2" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Gaming laptop</h5>
+                                        <p className="card-text">$59000</p>
+                                        <a href="#" className="btn btn-primary">BUY NOW</a>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://m.media-amazon.com/images/I/71EgfaUzwNL._AC_UY327_FMwebp_QL65_.jpg" className="card-img-top" alt="Card 3" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Gaming laptop</h5>
+                                        <p className="card-text">$59000</p>
+                                        <a href="#" className="btn btn-primary">BUY NOW</a>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://m.media-amazon.com/images/I/815ZrCdBiqL._AC_UY327_FMwebp_QL65_.jpg" className="card-img-top" alt="Card 3" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Gaming laptop</h5>
+                                        <p className="card-text">$59000</p>
+                                        <a href="#" className="btn btn-primary">BUY NOW</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://m.media-amazon.com/images/I/51qaPeW62BL._AC_UY327_FMwebp_QL65_.jpg" className="card-img-top" alt="Card 3" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Gaming laptop</h5>
+                                        <p className="card-text">$59000</p>
+                                        <a href="#" className="btn btn-primary">BUY NOW</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://m.media-amazon.com/images/I/8133GVuhgwL._AC_UY327_FMwebp_QL65_.jpg" className="card-img-top" alt="Card 3" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Gaming laptop</h5>
+                                        <p className="card-text">$59000</p>
+                                        <a href="#" className="btn btn-primary">BUY NOW</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://m.media-amazon.com/images/I/81u-5gRjEXL._AC_UY327_FMwebp_QL65_.jpg" className="card-img-top" alt="Card 3" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Gaming laptop</h5>
+                                        <p className="card-text">$59000</p>
+                                        <a href="#" className="btn btn-primary">BUY NOW</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://m.media-amazon.com/images/I/71+bSm87KmL._AC_UY327_FMwebp_QL65_.jpg" className="card-img-top" alt="Card 3" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Gaming laptop</h5>
+                                        <p className="card-text">$59000</p>
+                                        <a href="#" className="btn btn-primary">BUY NOW</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://m.media-amazon.com/images/I/71iyBsDDwQL._AC_UY327_FMwebp_QL65_.jpg" className="card-img-top" alt="Card 3" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Gaming laptop</h5>
+                                        <p className="card-text">$59000</p>
+                                        <a href="#" className="btn btn-primary">BUY NOW</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4 col-xxl-4">
+                                <div className="card" >
+                                    <img src="https://m.media-amazon.com/images/I/61TeuJCIVcL._AC_UY327_FMwebp_QL65_.jpg" className="card-img-top" alt="Card 3" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Gaming laptop</h5>
+                                        <p className="card-text">$59000</p>
+                                        <a href="#" className="btn btn-primary">BUY NOW</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ShoppingUI;
